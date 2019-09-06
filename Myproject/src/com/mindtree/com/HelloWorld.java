e387e56545d17bf0a71d185c6949ecfc98ea411d
Sai Dash <M1053759@mindtree.com>
2019-09-06 11:27:14 +0530

    project added

---
+++ Myproject/src/com/mindtree/com/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.mindtree.com;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("Hello Mindtree");
+		
+	}
+
+}
